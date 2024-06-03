44e37f2afe31cd14aa19410310f1a956073be81b
Nguyễn Đỗ Thế Nguyên <thenguyen1024@gmail.com>
2024-06-03 21:09:11 +0700

    address authorization

---
+++ app/Policies/DistrictPolicy.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace App\Policies;
+
+use App\Enums\UserRole;
+use App\Models\Province;
+use App\Models\User;
+use Illuminate\Auth\Access\Response;
+
+class DistrictPolicy
+{
+    /**
+     * Determine whether the user can view any models.
+     */
+    public function viewAny(User $user): Response
+    {
+        return $user->role === UserRole::Admin ? Response::allow() : Response::deny();
+    }
+
+    /**
+     * Determine whether the user can view the model.
+     */
+    public function view(User $user, Province $province): Response
+    {
+        return $user->role === UserRole::Admin ? Response::allow() : Response::deny();
+    }
+
+    /**
+     * Determine whether the user can create models.
+     */
+    public function create(User $user): Response
+    {
+        return $user->role === UserRole::Admin ? Response::allow() : Response::deny();
+    }
+
+    /**
+     * Determine whether the user can update the model.
+     */
+    public function update(User $user, Province $province): Response
+    {
+        return $user->role === UserRole::Admin ? Response::allow() : Response::deny();
+    }
+
+    /**
+     * Determine whether the user can delete the model.
+     */
+    public function delete(User $user, Province $province): Response
+    {
+        return $user->role === UserRole::Admin ? Response::allow() : Response::deny();
+    }
+
+    /**
+     * Determine whether the user can restore the model.
+     */
+    public function restore(User $user, Province $province): Response
+    {
+        return $user->role === UserRole::Admin ? Response::allow() : Response::deny();
+    }
+
+    /**
+     * Determine whether the user can permanently delete the model.
+     */
+    public function forceDelete(User $user, Province $province): Response
+    {
+        return $user->role === UserRole::Admin ? Response::allow() : Response::deny();
+    }
+}
